d2bb5e3dcd5b3d9dfac42d4e17f8543be1fd37ca
leenaars <michiel.ml@nlnet.nl>
2015-10-21 09:56:41 +0200

    Changed hashbang for broader distro compatibility + minor corrections

--- palea-express/Functions.py
+++ palea-express/Functions.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
+# See LICENSE.txt for details.
 # Version see CHANGELOG file
 
 import socket
 import re
 
 def isValidIp(ip):
-        """ A regular expression for checking the validity of an IP """
+        """ A regular expression for checking the validity of an IPv4 address """
 	pattern = r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
 	if re.match(pattern, ip):
 		return True
